032f0b4664dba98efcef9d23b927484dca4e3342
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2023-01-30 20:11:02 -0500

    Clean up, document , reorder functionsVAT/Rules.mk

--- functionsVAT/Rules.mk
+++ functionsVAT/Rules.mk
@@ -1,10 +1,10 @@
-PFs := VATDEF.FILE
-MODULEs := VAT300.MODULE
-SRVPGMs := FVAT.SRVPGM
-
-VAT300.MODULE: vat300.rpgle QPROTOSRC/vat.rpgleinc
+# Note we build everything to do with this Service Program in this directory
+# There is one dependency on the DB reference file SAMREF which is in the 
+# 'common' directory
 
 FVAT.SRVPGM: fvat.bnd VAT300.MODULE
 FVAT.SRVPGM: TEXT = Functions VAT
+
+VAT300.MODULE: vat300.rpgle vat.rpgleinc VATDEF.FILE
 
 VATDEF.FILE: vatdef.pf SAMREF.FILE
