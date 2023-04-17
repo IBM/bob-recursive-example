274ee8845bd2439efe7db642417eea05c6139691
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2023-04-17 18:39:29 -0400

    add tests for Rules.mk variables

--- functionsVAT/Rules.mk
+++ functionsVAT/Rules.mk
@@ -1,6 +1,13 @@
-VAT300.MODULE: vat300.rpgle QPROTOSRC/vat.rpgleinc
+# Note we build everything to do with this Service Program in this directory
+# There is one dependency on the DB reference file SAMREF which is in the 
+# 'common' directory
 
 FVAT.SRVPGM: fvat.bnd VAT300.MODULE
 FVAT.SRVPGM: TEXT = Functions VAT
+FVAT.SRVPGM: private TEXT = Functions VAT
+
+VAT300.MODULE: vat300.rpgle QPROTOSRC/vat.rpgleinc VATDEF.FILE
+VAT300.MODULE: private TEXT := bound into FVAT.SRVPGM
+VAT300.MODULE: private DBGVIEW ::= *SOURCE
 
 VATDEF.FILE: vatdef.pf SAMREF.FILE
